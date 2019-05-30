29dc3743917b7240e54aba0903c031e7c654a543
John Goodliff <johny112009@hotmail.com>
2019-05-29 18:53:17 -0600

    Removed incorrect screenshotts. Compressed images. Fixed styles not being applied correctly. Made modal more responsive. Incremented version number

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Copy files to build/, builds css file, and zips
-rm -rf build && mkdir -p build && cp README.md LICENSE.md manifest.json 128.png 48.png 32.png 16.png background.js content.js build/ && sass style.scss build/style.css --style=compressed --sourcemap=none && zip -rj build/tally-word-counter.zip build/* && echo "BUILD SUCCESSFUL"
+rm -rf build && mkdir -p build && cp README.md LICENSE manifest.json 128.png 48.png 32.png 16.png background.js content.js build/ && sass style.scss build/style.css --style=compressed --sourcemap=none && zip -rj build/tally-word-counter.zip build/* && echo "BUILD SUCCESSFUL"
